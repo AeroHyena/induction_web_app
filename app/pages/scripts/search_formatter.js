027e06eeee46cff330a0c0bbe97fb7046ae81189
Arnold Lohmann <rakudreemurr@gmail.com>
2023-10-12 10:05:00 +0200

    Updated the design and layout to be more mobile-freindly

--- app/pages/scripts/search_formatter.js
+++ app/pages/scripts/search_formatter.js
@@ -1,9 +1,18 @@
+/**
+ * This code looks at the time passed since each induction search result
+ * has been completed, and updates the search result to reflect the validity
+ * of the specific record.
+ */
+
+/** Select the dates on the records, as well as the records */
 let arrayDates = Array.from(document.getElementsByClassName("dateCompleted"));
 let arrayStatuses = Array.from(document.getElementsByClassName("status"));
 console.log("selected data ", arrayDates, "and ", arrayStatuses);
+
+
+/** Convert SQLite datetime to JavaScript Date object */
 let convertedDates = [];
 
-// Convert SQLite datetime to JavaScript Date object
 arrayDates.forEach(element => {
     convertedDates.push({
         date: new Date(element.innerHTML),
@@ -14,15 +23,17 @@ arrayDates.forEach(element => {
 console.log("Converted dates to JavScript date objects");
 console.log("Analyzing date values...")
 
-// Get current date and time
+
+/**  Get the current date and time */
 let currentDate = new Date();
 
-// Get the date and time for one year ago
+
+/** Get the date and time for one year ago */
 let oneYearAgo = new Date();
 oneYearAgo.setFullYear(currentDate.getFullYear() - 1);
 
 
-// Compare dates and add appropriate stylized text into the Status divs
+/** Compare dates, and add appropriate stylized text into the Status divs */
 convertedDates.forEach((item, index) => {
     console.log("Checking ", item);
 
