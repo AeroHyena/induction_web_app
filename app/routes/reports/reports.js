b8463793f1789a9a2202065ac730bbd72ebf8426
Arnold Lohmann <rakudreemurr@gmail.com>
2023-10-19 10:11:31 +0200

    v4.1.0 - added reports generation: - Added the ability to generate reports on the /reports routes - Added styling and layout definitions to the reports - Added logic to the /reports route to allow the ability to include or exclude data from reports

--- app/routes/reports/reports.js
+++ app/routes/reports/reports.js
@@ -14,11 +14,58 @@
 
 
 /* imports and set up*/
+const { time } = require("console");
 const express = require("express");
 const { todo } = require("node:test");
 const router = express.Router();
 
+/* Functions */
+function filterRecordsByExpiry(records) {
+     // set up date variables
 
+     // Current date
+     const currentDate = Date.now(); 
+
+     // Year
+     const oneYearAgo = new Date(currentDate);
+     oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+     const oneYearInMilliseconds = 365 * 24 * 60 * 60 * 1000
+     
+     // Week
+     const oneWeekFromNow = new Date(currentDate);
+     oneWeekFromNow.setDate(oneWeekFromNow.getDate() + 7);
+     const oneWeekInMilliseconds = 7 * 24 * 60 * 60 * 1000; // 7 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
+     
+     // Month
+     const oneMonthFromNow = new Date(currentDate);
+     oneMonthFromNow.setMonth(oneMonthFromNow.getMonth() + 1);
+     const oneMonthInMilliseconds = 30.44 * 24 * 60 * 60 * 1000; // 30.44 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
+
+     // Set up return variables
+     const expired = [], expiresWeek = [], expiresMonth = [];
+     
+     // Filter records
+     records.forEach((record) => {
+         const databaseTimestamp = Date.parse(record.date_completed);
+         
+         // Calculate the difference in milliseconds between databaseTimestamp and the current date
+         const timeDifference = currentDate - databaseTimestamp;
+         const timeLeft = oneYearInMilliseconds - timeDifference;
+         
+         if (timeDifference < oneYearInMilliseconds) { // if the record is not older than a year
+             if (timeLeft <= oneWeekInMilliseconds) { // check if it expires in the next week
+                 expiresWeek.push(record);
+             }
+             if (timeLeft <= oneMonthInMilliseconds) { // check if it expires in the next month
+                 expiresMonth.push(record);
+             }
+         } else {
+             expired.push(record);
+         }
+     });
+
+     return {expired, expiresWeek, expiresMonth};
+}
 
 
 /** 
@@ -42,7 +89,7 @@ module.exports = (db) => {
 
         // use template.ejs as base, and insert reports.ejs into the template page
         if (req.session.isLoggedIn) {
-            res.status(200).render("template", {loggedIn: req.session.isLoggedIn, title: "Reports", contentPath: "reports"});
+            res.status(200).render("template", {loggedIn: req.session.isLoggedIn, title: "Reports", contentPath: "reports", reportRecords: null});
             console.log("Reports: @/get - reports.ejs rendered @" + new Date());
         } else {
             res.redirect("/");
@@ -55,11 +102,17 @@ module.exports = (db) => {
     router.post("/", (req, res) => {
         // Read the data
         console.log("Reports: POST detected - reading data...");
-
-        let showExpired = (req) => req.body.showExpired === "on";
-        let showWeek = (req) => req.body.showWeek === "on";
-        let showMonth = (req) => req.body.showMonth === "on";
-
+        let showExpired = false, showWeek = false, showMonth = false;
+        if (req.body.showExpired === "on") {
+            showExpired = true
+        };
+        if (req.body.showWeek === "on") {
+            showWeek = true
+        };
+        if (req.body.showMonth === "on") {
+            showMonth = true
+        };
+        console.log(showExpired, showWeek, showMonth)
 
         // Check if any options were selected
         if (!showExpired && !showWeek && !showMonth) {
@@ -75,59 +128,19 @@ module.exports = (db) => {
                     console.log("error", err);
                 } else {
                     const data = rows.map((row) => row); // Use map to extract rows
-                
-            
-          
 
 
-                    // Filter the data
-                    const currentDate = Date.now();
-                    const oneYearAgo = new Date(currentDate);
-                    oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
-                    
-                    const oneWeekFromNow = new Date(currentDate);
-                    oneWeekFromNow.setDate(oneWeekFromNow.getDate() + 7);
-                    
-                    const oneMonthFromNow = new Date(currentDate);
-                    oneMonthFromNow.setMonth(oneMonthFromNow.getMonth() + 1);
-
-                    const oneWeekInMilliseconds = 7 * 24 * 60 * 60 * 1000; // 7 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
-                    const oneMonthInMilliseconds = 30.44 * 24 * 60 * 60 * 1000; // 30.44 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
-                    const oneYear = 365 * 24 * 60 * 60 * 1000
-
-                    const expired = [], expiresWeek = [], expiresMonth = [];
-                    
-                    data.forEach((record) => {
-                        const databaseTimestamp = Date.parse(record.date_completed);
-                        
-                        // Calculate the difference in milliseconds between databaseTimestamp and the current date
-                        const timeDifference = currentDate - databaseTimestamp;
-                        
-                        if (timeDifference < oneYear) {
-                            if (timeDifference < oneWeekInMilliseconds) {
-                                expiresWeek.push(record);
-                            }
-                            if (timeDifference < oneMonthInMilliseconds) {
-                                expiresMonth.push(record);
-                            }
-                        } else {
-                            expired.push(record);
-                        }
-                    });
-                    
-                    
-                    
-
-                    //console.log(expiresWeek, expiresMonth);
-
-                    res.send({expiresWeek, expiresMonth});
+                   const results = filterRecordsByExpiry(data);
+                   const render = {};
+                   if (showExpired) {Object.assign(render, {"Expired": results.expired})};
+                   if (showWeek) {Object.assign(render, {"Expires In A Week": results.expiresWeek})};
+                   if (showMonth) {Object.assign(render, {"Expires In A Month": results.expiresMonth})};
+                   console.log(render)
+                   
+                   res.status(200).render("template", {loggedIn: req.session.isLoggedIn, title: "Reports", contentPath: "reports", reportRecords: render});
+                   
                 }
             });
-        
-
-
-
-
     });
     
 
