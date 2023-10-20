9725fd72303176f0bdb0f0b88a7cf323d47c792b
Arnold Lohmann <rakudreemurr@gmail.com>
2023-10-20 11:02:25 +0200

    v5.1.0 - Updated the layout and styling of downloadable pdf's

--- app/pages/scripts/report_save_handler.js
+++ app/pages/scripts/report_save_handler.js
@@ -1,5 +1,7 @@
 window.jsPDF = window.jspdf.jsPDF;
 
+
+
 function extractReportData() {
     // Get the main report container by its ID
     const reportContainer = document.getElementById('report');
@@ -60,7 +62,7 @@ function downloadPDF() {
     const hasNoData = divToCapture.innerText.includes("Nothing to show");
   
     if (hasNoData) {
-      alert("No report data to email");
+      alert("No report data to download");
     } else {
       
   
@@ -70,32 +72,73 @@ function downloadPDF() {
 
         // construct raw data into pdf
         const doc = new jsPDF;
-        let x = 10;
-        let y = 10;
+        let x = 20;
+        let y = 20;
 
+        doc.setFont("times", "rowan", "normal");
+        doc.setFontSize(18);
+        doc.text("FSOil - Induction Expiry Report", x, y);
+        const currentDate = new Date();
+        doc.text(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`, x + 136, y);
+        doc.line(x, y + 1, x + 165, y + 1); // Adjust the y+1 value for the underline position
+        y += 18;
+
+        doc.setFontSize(12);
+        doc.text(["This is a generated report describing any records of performed inductions that has expired, and/or", 
+        "will expire soon."], x, y)
+        y += 18;
+
+
+        // Set up fonts to use in pdf
+        console.log(doc.getFontList());
 
         for (let category in extractedData) {
-        // Add a title
-        doc.setFontSize(12);
+        // Add a title with an underline
+        
+        doc.setFontSize(14);
         doc.text(category, x, y);
+        // Draw an underline below the category name
+        doc.line(x, y + 1, x + 85, y + 1); // Adjust the y+1 value for the underline position
         y += 12;
 
-        doc.setFontSize(10);
 
+        doc.setFontSize(10);
+          let column = 1;
         if (extractedData[category].length > 0) {
-            console.log(extractedData[category])
-            extractedData[category].forEach(dataPoint => {
+            
+            extractedData[category].forEach((dataPoint, index) => {
                 // Add datapoints
-                doc.text("Date Completed: " + dataPoint.DateCompleted, x, y);
-                y += 5;
-                doc.text("Full Names: " + dataPoint.FullNames, x, y);
-                y += 5;
-                doc.text("ID/Passport Number: " + dataPoint.IDOrPassport, x, y);
-                y += 5;
-                doc.text("Employee Number: " + dataPoint.EmployeeNumber, x, y); // Correct the variable name
-                y += 5;
-                doc.text("Video Watched: " + dataPoint.VideoWatched, x, y); // Correct the variable name.
-                y += 12;
+                if (column == 1) {
+                  doc.text(" - Date Completed: " + dataPoint.DateCompleted, x, y);
+                  y += 5;
+                  doc.text(" - Full Names: " + dataPoint.FullNames, x, y);
+                  y += 5;
+                  doc.text(" - ID/Passport Number: " + dataPoint.IDOrPassport, x, y);
+                  y += 5;
+                  doc.text(" - Employee Number: " + dataPoint.EmployeeNumber, x, y); // Correct the variable name
+                  y += 5;
+                  doc.text(" - Video Watched: " + dataPoint.VideoWatched, x, y); // Correct the variable name.
+                  
+                  if (index == extractedData[category].length - 1) { y += 12 } else {
+                    y -= 20;
+                  }
+                  column++;
+
+                } else {
+                  x += 100;
+                  doc.text(" - Date Completed: " + dataPoint.DateCompleted, x, y);
+                  y += 5;
+                  doc.text(" - Full Names: " + dataPoint.FullNames, x, y);
+                  y += 5;
+                  doc.text(" - ID/Passport Number: " + dataPoint.IDOrPassport, x, y);
+                  y += 5;
+                  doc.text(" - Employee Number: " + dataPoint.EmployeeNumber, x, y); // Correct the variable name
+                  y += 5;
+                  doc.text(" - Video Watched: " + dataPoint.VideoWatched, x, y); // Correct the variable name.
+                  y += 12;
+                  x -= 100;
+                  column--;
+                }
             });
         } else {
             doc.text("No data available.", x, y);
