58042ed7a9a450c3c7eebed573fbd1aaca8df0c0
Arnold Lohmann <rakudreemurr@gmail.com>
2023-10-24 14:50:24 +0200

    v6.0.0 - added /group_inductions: - group_inductions allows a logged in user to submit multiple inductions in one go. - You have to be logged in to be able to access group_inductions. - Stylized the route to maintain a table-like structure with inputs - Added back-end logic that reads the data and adds the new records  to the database.

--- app/app.js
+++ app/app.js
@@ -176,6 +176,7 @@ console.log("App: set up complete.");
 /* import and use routes */
 /** Import route modules and pass in the database connec connection to the route modules */
 const inductionRoutes = require("./routes/induction/induction")(app);
+const GroupInductionsRoutes = require("./routes/group_inductions/group_inductions.js")(app);
 const searchRoutes = require("./routes/search/search.js")(app);
 const reportsRoutes = require("./routes/reports/reports.js")(app);
 const loginRoutes = require("./routes/login/login.js")(app);
@@ -186,6 +187,7 @@ const dashboardRoutes = require("./routes/dashboard/dashboard.js")(app);
 
 /** Use the Route modules */
 app.use("/", inductionRoutes);
+app.use("/group_inductions", GroupInductionsRoutes);
 app.use("/search", searchRoutes);
 app.use("/reports", reportsRoutes);
 app.use("/login", loginRoutes);
